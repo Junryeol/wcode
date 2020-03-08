249482fb12f3f598161ce68b76e6f12d918fd76c
Junryeol <wnsfuf90@gmail.com>
2020-03-08 22:31:07 +0900

    add build dir

---
+++ build/precache-manifest.59fda5ff7db940d549f29af43a7a9c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3e94f0bff7953485e0cd85be2b8ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c652af502ad71889c2c",
+    "url": "/static/css/main.6b936516.chunk.css"
+  },
+  {
+    "revision": "720abbd85ec0ba356c40",
+    "url": "/static/js/2.3e6c851b.chunk.js"
+  },
+  {
+    "revision": "3d7b710e2b732e7d9f714b337a3038f4",
+    "url": "/static/js/2.3e6c851b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c652af502ad71889c2c",
+    "url": "/static/js/main.0dc1caf6.chunk.js"
+  },
+  {
+    "revision": "e1414836b2137270af64",
+    "url": "/static/js/runtime-main.8989d059.js"
+  }
+]);
